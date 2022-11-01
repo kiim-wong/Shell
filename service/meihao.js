abbf05cd79475a2049a15a8ec0ef638dcd07bb4f
kiim-wong <guili_008@foxmail.com>
2022-11-01 18:34:10 +0800

    Update meihao.js

--- service/meihao.js
+++ service/meihao.js
@@ -1,20 +1,15 @@
-/*******************************
+/*
+黄油相机
 
-*******************************
+>Gx3dong制作,免费资源群:1077223830.进群不迷路.
+>木木团队,欢迎关注公众号:十三座州府
+
 [rewrite_local]
-^http[s]?:\/\/api.wx.gcihotel.net\/\/api\/\/shop\/flashGoodsDetail.+$ url script-response-body https://raw.githubusercontent.com/kiim-wong/Shell/master/service/meihao.js
+^https:\/\/api\.wx\.gcihotel\.net\/api\/shop\/flashGoodsDetail\.json url script-response-body https://raw.githubusercontent.com/kiim-wong/Shell/master/service/meihao.js
+
 [mitm] 
-hostname = *.gcihotel.*
-*******************************
-Surge
+hostname = api.wx.gcihotel.net
+*/
 
-[Script]
-^http[s]?:\/\/api.wx.gcihotel.net\/\/api\/\/shop\/flashGoodsDetail.+$ url script-response-body https://raw.githubusercontent.com/kiim-wong/Shell/master/service/meihao.js
-
-[MITM]
-hostname = *.gcihotel.*
-
-*******************************/
-var obj = JSON.parse($response.body);
-    obj.data.retVal.flashGoods.dayStartTime = "2022-11-01 10:00:00";
-    $done({body: JSON.stringify(obj)});
+body = $response.body.replace("20:00:00", "10:00:00")
+$done({body});
