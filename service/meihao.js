e3154ebcac4fc7a3cd9a282f90a79efd9209e242
kiim-wong <guili_008@foxmail.com>
2022-11-01 18:15:35 +0800

    Update meihao.js

--- service/meihao.js
+++ service/meihao.js
@@ -2,20 +2,19 @@
 
 *******************************
 [rewrite_local]
-^http[s]?:\/\/api.wx.gcihotel.net\/\/api\/\/shop\/flashGoodsDetail.+$ url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/Ceshi.js
+^http[s]?:\/\/api.wx.gcihotel.net\/\/api\/\/shop\/flashGoodsDetail.+$ url script-response-body https://raw.githubusercontent.com/kiim-wong/Shell/master/service/meihao.js
 [mitm] 
 hostname = *.gcihotel.*
 *******************************
 Surge
 
 [Script]
-^http[s]?:\/\/api.wx.gcihotel.net\/\/api\/\/shop\/flashGoodsDetail.+$ url script-response-body https://raw.githubusercontent.com/hhse/Mul4hong/master/Ceshi.js
+^http[s]?:\/\/api.wx.gcihotel.net\/\/api\/\/shop\/flashGoodsDetail.+$ url script-response-body https://raw.githubusercontent.com/kiim-wong/Shell/master/service/meihao.js
 
 [MITM]
 hostname = *.gcihotel.*
 
 *******************************/
 var obj = JSON.parse($response.body);
-    obj.data.userId = 9999;
-obj.data.endTime = "脚本成功啦";
+    obj.data.retVal.flashGoods.dayStartTime = "2022-11-01 10:00:00";
     $done({body: JSON.stringify(obj)});
